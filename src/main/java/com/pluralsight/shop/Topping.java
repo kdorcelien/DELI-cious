5836e29db7673699ff3e7e236b4177898343002e
kdorcelien <kdorcelien.kd@gmail.com>
2025-11-07 14:30:45 -0500

    commit 5

--- src/main/java/com/pluralsight/shop/Topping.java
+++ src/main/java/com/pluralsight/shop/Topping.java
@@ -1,4 +1,26 @@
 package com.pluralsight.shop;
 
-public class Topping {
+public abstract class Topping {
+    protected String name;
+    protected boolean isPremium;
+
+    public Topping(String name, boolean isPremium) {
+        this.name = name;
+        this.isPremium = isPremium;
+    }
+
+    public abstract double piceBySize(String Size);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
 }
