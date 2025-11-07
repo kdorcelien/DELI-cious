dd4dc8936249db58f5009854c58dce591718f9bf
kdorcelien <kdorcelien.kd@gmail.com>
2025-11-07 15:21:50 -0500

    commit 9

--- src/main/java/com/pluralsight/shop/Chips.java
+++ src/main/java/com/pluralsight/shop/Chips.java
@@ -1,5 +1,21 @@
 package com.pluralsight.shop;
 
-public class Chips {
+import com.pluralsight.constant.Product;
 
+public class Chips implements Product {
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getName() {
+        return type + " Chips for: ";
+    }
+
+    @Override
+    public double getPrice(String size) {
+        return 1.50;
+    }
 }
