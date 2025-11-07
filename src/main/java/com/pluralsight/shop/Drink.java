28864292621500b903a21ceab485eb2fd0d0b756
kdorcelien <kdorcelien.kd@gmail.com>
2025-11-07 15:16:07 -0500

    commit 8

--- src/main/java/com/pluralsight/shop/Drink.java
+++ src/main/java/com/pluralsight/shop/Drink.java
@@ -1,4 +1,28 @@
 package com.pluralsight.shop;
 
-public class Drink {
+import com.pluralsight.constant.Product;
+
+public class Drink implements Product {
+    private String size, flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public String getName() {
+        return size + "" + flavor + " For: ";
+    }
+
+    @Override
+    public double getPrice(String size) {
+        return switch (size){
+            case "Small" -> 2.00;
+            case "Medium" -> 2.50;
+            case "Large" -> 3.00;
+            default -> 0.0;
+        };
+    }
 }
+
