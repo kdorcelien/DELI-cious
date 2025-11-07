30ad50c4f11ec46ba02f4891d4f2a549b24cfc70
kdorcelien <kdorcelien.kd@gmail.com>
2025-11-07 15:07:06 -0500

    commit 7

--- src/main/java/com/pluralsight/shop/RegularTopping.java
+++ src/main/java/com/pluralsight/shop/RegularTopping.java
@@ -1,4 +1,14 @@
 package com.pluralsight.shop;
 
-public class RegularTopping {
+public class RegularTopping extends Topping {
+
+    public RegularTopping(String name, boolean isPremium, boolean isExtra) {
+        super(name, false, false);
+    }
+
+    @Override
+    public double piceBySize(String size) {
+        System.out.println("Included");
+        return 0;
+    }
 }
